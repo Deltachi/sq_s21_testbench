d81bfd2580c9cab7f710c13c1cb888c181498c7a
Dominik <11911481+Deltachi@users.noreply.github.com>
2021-06-27 17:08:28 +0200

    Delete answer implemented for cleaner tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -62,6 +62,32 @@ async function startTest() {
 	}
 }
 
+async function deleteAnswer(test: string = ""): Promise<boolean> {
+	try {
+		const dropdown = await driver.findElement(By.id("ilAdvSelListAnchorText_QuestionActions"));
+		//await driver.wait(until.elementIsVisible(dropdown), 2000);
+		//dropdown.click(); //overlapping elements buggggging regular click out
+		await sleep(500); // alternative way
+		driver.executeScript("arguments[0].click();", dropdown); // alternative way
+		const deleteBtn = await driver.findElement(By.id("tst_discard_solution_action"));
+		await driver.wait(until.elementIsVisible(deleteBtn), 2000);
+		const deleteClasses = await deleteBtn.findElement(By.xpath("..")).getAttribute("class");
+		// wenn das li element (eltern) disabled ist kann die Aufgabe nicht gelöscht werden
+		if (deleteClasses.indexOf("disabled") >= 0) {
+			return false;
+		}
+		deleteBtn.click();
+		await sleep(500);
+		await driver.wait(until.elementLocated(By.name("cmd[discardSolution]")), 1000);
+		await driver.findElement(By.name("cmd[discardSolution]")).click();
+		await sleep(1000);
+		return true;
+	} catch (err) {
+		console.error(`DELETE ANSWER (test=${test}): ${err}`);
+		return false;
+	}
+}
+
 beforeAll(async () => {
 	driver = await createDriver(); // bot init
 	await login(); // login to ilias
@@ -75,15 +101,26 @@ afterAll(async () => {
 	await driver.quit(); // browser schließen
 });
 
-/**
- * Dieser Test schlägt fehl, weil ILIAS den Editor Content nicht speichert beim Klicken auf Speichern, wenn das Fenster 3s danach neu geladen wird.
- */
 test("Fill freeform, click save and immediate reload", async () => {
-	const result = await fillFreeFormSaveAndReload("Hallo das ist ein Text, genereiert von Selenium!");
-	expect(result).toBe("Hallo das ist ein Text, genereiert von Selenium!");
+	const result = await fillFreeFormSaveReloadAndCheck("Hallo das ist ein Text, genereiert von Selenium!");
+	expect(result).toBe("Hallo das ist ein Text, genereiert von Selenium!"); // failed, weil Speichern NICHT speichert
 }, 20000);
 
-async function fillFreeFormSaveAndReload(text: string) {
+test("Mark text switch to overview and back", async () => {
+	const result = await markWordsReloadAndCheck();
+	expect(result).toBeTruthy(); // sollte wahr sein, weil die Auswahl gespeichert wird
+}, 20000);
+
+//###########################
+// BEGIN TEST SUB-FUNCTIONS #
+//###########################
+//
+/**
+ * Fügt Text in ein Freitextfeld ein, klickt auf speichern und lädt die Seite neu
+ * @param text Testtext welcher eingefügt werden soll
+ * @returns Text innerhalb des Editors nach dem Test
+ */
+async function fillFreeFormSaveReloadAndCheck(text: string) {
 	let result = null;
 	try {
 		await startTest();
@@ -92,7 +129,10 @@ async function fillFreeFormSaveAndReload(text: string) {
 		await driver.findElement(By.id("listofquestions")).findElement(By.linkText("Freitext")).click();
 
 		await driver.wait(until.elementsLocated(By.css("iframe.tox-edit-area__iframe")), 30000);
-		await driver.switchTo().frame(driver.findElement(By.css("iframe.tox-edit-area__iframe"))); // switch to iframe
+
+		await deleteAnswer();
+
+		await driver.switchTo().frame(driver.findElement(By.css("iframe.tox-edit-area__iframe"))); // switch to iframe to input into tiny-mce editor
 
 		await driver.findElement(By.css(".mce-content-body")).sendKeys(text);
 
@@ -115,17 +155,15 @@ async function fillFreeFormSaveAndReload(text: string) {
 	return result;
 }
 
-test("Mark text switch to overview and back", async () => {
-	const result = await markWords();
-	expect(result).toBeTruthy();
-}, 20000);
-
-async function markWords(): Promise<boolean> {
+async function markWordsReloadAndCheck(): Promise<boolean> {
 	try {
 		await startTest();
 		await driver.wait(until.elementsLocated(By.id("listofquestions")), 30000);
 		await driver.findElement(By.id("listofquestions")).findElement(By.linkText("Worte markieren")).click();
 		await driver.wait(until.elementsLocated(By.linkText("falsch")), 30000);
+
+		await deleteAnswer();
+
 		await (
 			await driver.findElements(By.linkText("falsch"))
 		).forEach(async (element) => {
